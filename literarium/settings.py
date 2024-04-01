9a91763218aefc75aa64744ef26322fc97f31014
Cristiano Tobias <cristiano.tobias40@gmail.com>
2024-04-01 19:12:15 -0300

    Adicionado paginação e debug_toolbar ao projeto

--- literarium/settings.py
+++ literarium/settings.py
@@ -37,10 +37,11 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "rest_framework",  # Adicionado o Django REST Framework
     "django_extensions",
+    "rest_framework",  # Adicionado o Django REST Framework
     "order",
     "product",
+    "debug_toolbar",
     
 ]
 
@@ -52,6 +53,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
 ROOT_URLCONF = "literarium.urls"
@@ -126,3 +128,12 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+INTERNAL_IPS = [
+    '127.0.0.1',
+]
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 5
+}

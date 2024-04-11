2e741521f97939650c1c60af8eff9dc4fca555c2
Cristiano Tobias <cristiano.tobias40@gmail.com>
2024-04-11 09:07:03 -0300

    Adiciona suporte Docker Compose, Makefile e Black

--- order/tests/test_viewsets/test_order_viewset.py
+++ order/tests/test_viewsets/test_order_viewset.py
@@ -8,6 +8,7 @@ from order.factories import UserFactory, OrderFactory
 from product.models import Product
 from order.models import Order
 
+
 class TestOrderViewSet(APITestCase):
     client = APIClient()
 
@@ -15,38 +16,52 @@ class TestOrderViewSet(APITestCase):
         self.user = UserFactory()
         token = Token.objects.create(user=self.user)
         token.save()
-        self.category = CategoryFactory(title= 'technology')
-        self.product = ProductFactory(title='mouse', price=100, category=[self.category])
+        self.category = CategoryFactory(title="technology")
+        self.product = ProductFactory(
+            title="mouse", price=100, category=[self.category]
+        )
         self.order = OrderFactory(product=[self.product])
 
     def test_order(self):
         token = Token.objects.get(user__username=self.user.username)
-        self.client.credentials(HTTP_AUTHORIZATION='Token ' + token.key)
-        response = self.client.get(reverse('order-list', kwargs={'version': 'v1'}))
+        self.client.credentials(HTTP_AUTHORIZATION="Token " + token.key)
+        response = self.client.get(reverse("order-list", kwargs={"version": "v1"}))
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         order_data = json.loads(response.content)
-        self.assertEqual(order_data['results'][0]['product'][0]['title'], self.product.title)
-        self.assertEqual(order_data['results'][0]['product'][0]['price'], self.product.price)
-        self.assertEqual(order_data['results'][0]['product'][0]['active'], self.product.active)
-        self.assertEqual(order_data['results'][0]['product'][0]['category'][0]['title'], self.category.title)
+        self.assertEqual(
+            order_data["results"][0]["product"][0]["title"], self.product.title
+        )
+        self.assertEqual(
+            order_data["results"][0]["product"][0]["price"], self.product.price
+        )
+        self.assertEqual(
+            order_data["results"][0]["product"][0]["active"], self.product.active
+        )
+        self.assertEqual(
+            order_data["results"][0]["product"][0]["category"][0]["title"],
+            self.category.title,
+        )
 
     def test_create_order(self):
         token = Token.objects.get(user__username=self.user.username)
-        self.client.credentials(HTTP_AUTHORIZATION='Token ' + token.key)
+        self.client.credentials(HTTP_AUTHORIZATION="Token " + token.key)
         user = UserFactory()
         product = ProductFactory()
-        data = json.dumps({
-            'products_id': [product.id],
-            'user': user.id
-        })
-        response = self.client.post(reverse('order-list', kwargs={'version': 'v1'}), data=data, content_type='application/json')
+        data = json.dumps({"products_id": [product.id], "user": user.id})
+        response = self.client.post(
+            reverse("order-list", kwargs={"version": "v1"}),
+            data=data,
+            content_type="application/json",
+        )
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         created_order = Order.objects.get(user=user)
-        
+
     def test_delete_order(self):
         token = Token.objects.get(user__username=self.user.username)
-        self.client.credentials(HTTP_AUTHORIZATION='Token ' + token.key)
+        self.client.credentials(HTTP_AUTHORIZATION="Token " + token.key)
         # Test to delete an order
-        response = self.client.delete(reverse('order-detail', kwargs={'version': 'v1', 'pk': self.order.pk}))
+        response = self.client.delete(
+            reverse("order-detail", kwargs={"version": "v1", "pk": self.order.pk})
+        )
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
         self.assertFalse(Order.objects.filter(pk=self.order.pk).exists())
--- wait/models/serializer_test.py
+++ wait/models/serializer_test.py
@@ -6,17 +6,18 @@ from order.serializers import OrderSerializer
 from product.models import Product
 from django.contrib.auth.models import User
 
+
 class OrderSerializerTestCase(TestCase):
     def setUp(self):
         # Crie alguns produtos de exemplo para usar nos testes
-        self.product1 = Product.objects.create(title='Product 1', price=10)
-        self.product2 = Product.objects.create(title='Product 2', price=20)
+        self.product1 = Product.objects.create(title="Product 1", price=10)
+        self.product2 = Product.objects.create(title="Product 2", price=20)
 
         # Crie um usuário de exemplo
-        self.user = User.objects.create(username='test_user')
+        self.user = User.objects.create(username="test_user")
 
         # Crie um pedido de exemplo com os produtos e o usuário
-        self.order = Order.objects.create(user=self.user, status='Pending')
+        self.order = Order.objects.create(user=self.user, status="Pending")
         self.order.product.add(self.product1, self.product2)
 
     def test_order_serializer(self):
@@ -25,4 +26,4 @@ class OrderSerializerTestCase(TestCase):
 
         # Verificar se a soma dos preços dos produtos está correta
         total = self.product1.price + self.product2.price
-        self.assertEqual(serializer.data['total'], total)
+        self.assertEqual(serializer.data["total"], total)

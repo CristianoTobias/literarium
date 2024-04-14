48ed33bb286974e56ccafad83494e668c68a04b8
Cristiano Tobias <cristiano.tobias40@gmail.com>
2024-04-14 00:49:05 -0300

    fix env.dev django_allowed_hosts

--- literarium/settings.py
+++ literarium/settings.py
@@ -158,4 +158,5 @@ SECRET_KEY = os.environ.get("SECRET_KEY", SECRET_KEY)
 
 DEBUG = int(os.environ.get("DEBUG", default=0))
 
-ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "").split(",")
+#ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "").split(",")
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "ebac-literarium-api-6e54c265fcd3.herokuapp.com", "::1"]
